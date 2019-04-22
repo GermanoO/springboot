bbaa71dc1dc4dae078bf5d994503dc32bb700f9b
Germano <bruno@usto.re>
2019-04-22 01:00:01 -0300

    criacao automatica da base de dados

--- src/main/java/com/curso/springboot/domain/Categoria.java
+++ src/main/java/com/curso/springboot/domain/Categoria.java
@@ -1,12 +1,19 @@
 package com.curso.springboot.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
-
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = -8830987102395604082L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
