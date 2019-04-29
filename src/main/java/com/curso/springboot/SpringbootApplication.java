569541c73dc673416f3c08053cd92c7b5d54213c
germanoo <bgermano89@gmail.com>
2019-04-29 01:04:40 -0300

    Produto e Categoria associação n-n

--- src/main/java/com/curso/springboot/SpringbootApplication.java
+++ src/main/java/com/curso/springboot/SpringbootApplication.java
@@ -1,7 +1,9 @@
 package com.curso.springboot;
 
 import com.curso.springboot.domain.Categoria;
+import com.curso.springboot.domain.Produto;
 import com.curso.springboot.repository.CategoriaRepository;
+import com.curso.springboot.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class SpringbootApplication implements CommandLineRunner {
     @Autowired
     CategoriaRepository categoriaRepository;
 
+    @Autowired
+    ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
@@ -26,6 +31,18 @@ public class SpringbootApplication implements CommandLineRunner {
         Categoria cat2 = new Categoria(null, "Escritório");
         Categoria cat3 = new Categoria(null, "Cama");
 
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "IMpressora", 800.00);
+        Produto p3 = new Produto(null, "mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+        cat2.getProdutos().add(p3);
+
+        p1.getCategorias().add(cat1);
+        p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+        p3.getCategorias().add(cat1);
+
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 }
